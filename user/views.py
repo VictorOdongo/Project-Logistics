7cdb402ee0005096e914e84570d291bbe377f27a
VictorOdongo <victorodongo397@gmail.com>
2023-05-20 11:15:47 +0300

    driver update

--- user/views.py
+++ user/views.py
@@ -8,3 +8,9 @@ def getstarted_view(request):
 
 def personal_view(request):
     return render(request, 'user/personal-signup.html')
+
+def sender_view(request):
+    return render(request, 'user/sender-signup.html')
+
+def driver_view(request):
+    return render(request, 'user/driver-signup.html')
