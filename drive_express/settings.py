3df17847f847af403d19e86bb6a48b055010b4d8
VictorOdongo <victorodongo397@gmail.com>
2023-05-23 14:23:45 +0300

    postgresql

--- drive_express/settings.py
+++ drive_express/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'drive_express.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'students',
+        'USER': 'postgres',
+        'PASSWORD': 'victor100',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
