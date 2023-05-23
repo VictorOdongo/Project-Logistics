b17a4e2bcdd49df87795b72f123833d4c277f53c
VictorOdongo <victorodongo397@gmail.com>
2023-05-23 15:25:51 +0300

    database

--- drive_express/urls.py
+++ drive_express/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
+    path('user/', include('django.contrib.auth.urls')),
+
 ]
--- user/views.py
+++ user/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 def home_view(request):
     return render(request, 'home.html')
@@ -14,3 +16,25 @@ def sender_view(request):
 
 def driver_view(request):
     return render(request, 'user/driver-signup.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirect to your desired page
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Redirect to your desired page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
