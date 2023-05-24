4c44ba3ef7b707caca8649cb61123ccd6e4cbcda
VictorOdongo <victorodongo397@gmail.com>
2023-05-24 17:40:02 +0300

    sign in

--- drive_express/urls.py
+++ drive_express/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
     path('user/', include('django.contrib.auth.urls')),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
