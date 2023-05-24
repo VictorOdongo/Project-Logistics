d6094644fa18a11ba6008def3c9ba21705918b7d
VictorOdongo <victorodongo397@gmail.com>
2023-05-24 15:30:52 +0300

    view page

--- user/urls.py
+++ user/urls.py
@@ -6,6 +6,11 @@ app_name = 'user'
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('getstarted/', views.getstarted_view, name='getstarted'),
-    path('getstarted/', views.getstarted_view, name='getstarted'),
-    path('getstarted/', views.getstarted_view, name='getstarted'),    
+    path('personal/', views.personal_view, name='personal-signup'),
+    path('business/', views.business_view, name='business-signup'), 
+    path('driver/', views.driver_view, name='driver-signup'), 
+    path('drive/', views.drive_view, name='driver-login'), 
+    path('send/', views.send_view, name='sender-login'),    
+    path('sendgig/', views.sendgig_view, name='send'), 
+    path('drivegig/', views.drivegig_view, name='drive'), 
 ]
