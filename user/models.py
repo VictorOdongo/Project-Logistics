d174d588963a0a548e82e886add2a653216f0d5d
VictorOdongo <victorodongo397@gmail.com>
2023-05-26 10:34:39 +0300

    authentication

--- user/models.py
+++ user/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
@@ -10,7 +11,7 @@ class Personal(models.Model):
     id = models.AutoField(primary_key=True, blank=False,unique=True)
     mobile = models.IntegerField(null=False, blank=False)
     email = models.EmailField(max_length=20, blank=False, unique=True)
-    password = models.CharField(max_length=20, blank=False, unique=True)
+    password = models.CharField(blank=False)
     
 class Entreprise(models.Model):
     businessname = models.CharField(max_length=20, blank=False, unique=True)
@@ -20,7 +21,7 @@ class Entreprise(models.Model):
     id = models.AutoField(primary_key=True, blank=False,unique=True)
     mobile = models.IntegerField(null=False, blank=False)
     email = models.EmailField(max_length=20, blank=False, unique=True)
-    password = models.CharField(max_length=20, blank=False, unique=True)
+    password = models.CharField(blank=False)
     
 class Driver(models.Model):
     firstname = models.CharField(max_length=10, blank=False)
@@ -29,7 +30,7 @@ class Driver(models.Model):
     licence = models.CharField(max_length=10, blank=False, primary_key=True, unique=True)
     mobile = models.IntegerField(null=False, blank=False)
     email = models.EmailField(max_length=20, blank=False, unique=True)
-    password = models.CharField(max_length=20, blank=False, unique=True)
+    password = models.CharField(blank=False)
                 
     
     
--- user/views.py
+++ user/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.contrib import messages, auth
-from user import Personal
 
 
 def home_view(request):
