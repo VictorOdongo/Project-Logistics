d31d1283900161bd64f331b082de7ab12c63b954
VictorOdongo <victorodongo397@gmail.com>
2023-05-29 22:57:02 +0300

    registration

--- user/models.py
+++ user/models.py
@@ -7,10 +7,11 @@ User = get_user_model()
 
 class Personal(models.Model):
     username = models.CharField(max_length=10, unique=True, blank=False, default='xx234xx')
+    firstname = models.CharField(max_length=10, blank=True)
     lastname = models.CharField(max_length=10, blank=False)
-    mobile = models.IntegerField(null=False, blank=False)
     email = models.EmailField(max_length=50, blank=False, unique=True, primary_key=True)
     password = models.CharField(max_length=255, blank=False)
+    password2 = models.CharField(max_length=255, blank=False)
     
 class Entreprise(models.Model):
     businessname = models.CharField(null=False, max_length=20, unique=True)
