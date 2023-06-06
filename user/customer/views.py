958ef870a64d94a57f33370da587d8b0656df9a7
VictorOdongo <victorodongo397@gmail.com>
2023-06-06 20:07:05 +0300

    customer

---
+++ user/customer/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+@login_required()
+def home(request):
+    return render(request, 'sendgig.html')
