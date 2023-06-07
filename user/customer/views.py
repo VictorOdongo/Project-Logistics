c223b705f75e47b98404504071ef84589f327282
VictorOdongo <victorodongo397@gmail.com>
2023-06-07 13:05:04 +0300

    customer

--- user/customer/views.py
+++ user/customer/views.py
@@ -3,4 +3,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required()
 def home(request):
-    return render(request, 'sendgig.html')
+    return render(request, 'customer/base.html')
