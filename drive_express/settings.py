a060969e32bdf7672345eb2a9b013e140fae3f34
VictorOdongo <victorodongo397@gmail.com>
2023-06-09 17:27:17 +0300

    email welcome

--- drive_express/settings.py
+++ drive_express/settings.py
@@ -143,3 +143,11 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # AUTH_USER_MODEL = 'user.Personal'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ['victorodongo397@gmail.com']
+EMAIL_HOST_PASSWORD = os.environ['tenacity100']
+DEFAULT_FROM_EMAIL = 'Fast Parcel <no-reply@drive_express.localhost>'
