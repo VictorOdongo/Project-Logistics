bb95f4ce78b87ba4d34b2f2d0e86ef64a8267c10
VictorOdongo <victorodongo397@gmail.com>
2023-06-12 20:30:18 +0300

    authentication

--- user/signals.py
+++ user/signals.py
@@ -2,8 +2,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
-from django.contrib.auth import User
+from django.contrib.auth import get_user_model
 from django.template.loader import render_to_string
+
+User = get_user_model()
 
 @receiver(post_save, sender=User)
 def send_welcome_email(sender, instance, created, **kwargs):
--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout as auth_logout
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib import messages
 from django.conf import settings
 from django.core.mail import send_mail
