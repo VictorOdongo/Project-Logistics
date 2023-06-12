b5b8169d60450cc8fb6108afe9c34765f497585e
VictorOdongo <victorodongo397@gmail.com>
2023-06-12 14:56:34 +0300

    settings

--- drive_express/settings.py
+++ drive_express/settings.py
@@ -149,5 +149,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ['victorodongo397@gmail.com']
-EMAIL_HOST_PASSWORD = os.environ['tenacity100']
+EMAIL_HOST_PASSWORD = os.environ['']
 DEFAULT_FROM_EMAIL = 'Fast Parcel <no-reply@drive_express.localhost>'
