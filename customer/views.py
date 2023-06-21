6744c481129f4362ae3bd7a555e94bf0ab0fe394
VictorOdongo <victorodongo397@gmail.com>
2023-06-21 21:34:28 +0300

    pickup address

--- customer/views.py
+++ customer/views.py
@@ -1,4 +1,6 @@
 # import requests
+# import stripe
+# import firebase_admin
 
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
@@ -72,7 +74,7 @@ def create_gig(request):
     creating_job = Job.objects.filter(customer=current_customer, status=Job.CREATING_STATUS).last()
     step1_form = forms.JobCreateStep1Form(instance=creating_job)
     step2_form = forms.JobCreateStep2Form(instance=creating_job)
-    step3_form = forms.JobCreateStep3Form(instance=creating_job)
+    # step3_form = forms.JobCreateStep3Form(instance=creating_job)
 
     
     if request.method == "POST":
@@ -95,63 +97,63 @@ def create_gig(request):
             if step3_form.is_valid():
                 creating_job = step3_form.save()
 
-                try:
-                    r = requests.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins={}&destinations={}&mode=transit&key={}".format(
-                    creating_job.pickup_address,
-                    creating_job.delivery_address,
-                    settings.GOOGLE_MAP_API_KEY,
-                ))
-                    print(r.json()['rows'])
+                # try:
+                #     r = requests.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins={}&destinations={}&mode=transit&key={}".format(
+                #     creating_job.pickup_address,
+                #     creating_job.delivery_address,
+                #     settings.GOOGLE_MAP_API_KEY,
+                # ))
+                #     print(r.json()['rows'])
                     
-                    distance = r.json()['rows'][0]['elements'][0]['distance']['value']
-                    duration = r.json()['rows'][0]['elements'][0]['duration']['value']
-                    creating_job.distance = round(distance / 1000, 2)
-                    creating_job.duration = int(duration / 60)
-                    creating_job.price = creating_job.distance * 1  # $1 per Km
-                    creating_job.save()
+                #     distance = r.json()['rows'][0]['elements'][0]['distance']['value']
+                #     duration = r.json()['rows'][0]['elements'][0]['duration']['value']
+                #     creating_job.distance = round(distance / 1000, 2)
+                #     creating_job.duration = int(duration / 60)
+                #     creating_job.price = creating_job.distance * 1  # $1 per Km
+                #     creating_job.save()
                                          
-                except Exception as e:
-                    print(e)
-                    messages.error(request, "Unfortunately, we do not support deliveries at this distance.")
+                # except Exception as e:
+                #     print(e)
+                #     messages.error(request, "Unfortunately, we do not support deliveries at this distance.")
                
                 return redirect(reverse('customer:create_gig'))
             
-        elif request.POST.get('step') == '4':
-            if creating_job.price:
-                try:
-                    # payment_intent = stripe.PaymentIntent.create(
-                    #     amount=int(creating_job.price * 100),
-                    #     currency='usd',
-                    #     automatic_payment_methods={"enabled": True},
-                    #     customer=current_customer.stripe_customer_id,
-                    #     payment_method=current_customer.stripe_payment_method_id,
-                    #     off_session=True,
-                    #     confirm=True,
-                    # )
+        # elif request.POST.get('step') == '4':
+        #     if creating_job.price:
+        #         try:
+        #             payment_intent = stripe.PaymentIntent.create(
+        #                 amount=int(creating_job.price * 100),
+        #                 currency='usd',
+        #                 automatic_payment_methods={"enabled": True},
+        #                 customer=current_customer.stripe_customer_id,
+        #                 payment_method=current_customer.stripe_payment_method_id,
+        #                 off_session=True,
+        #                 confirm=True,
+        #             )
                     
-                    # Transaction.objects.create(
-                    #     stripe_payment_intent_id = payment_intent['id'],
-                    #     job = creating_job,
-                    #     amount = creating_job.price,
-                    # )
+        #             Transaction.objects.create(
+        #                 stripe_payment_intent_id = payment_intent['id'],
+        #                 job = creating_job,
+        #                 amount = creating_job.price,
+        #             )
                     
-                    # creating_job.status = Job.PROCESSING_STATUS
-                    # creating_job.save()
+        #             creating_job.status = Job.PROCESSING_STATUS
+        #             creating_job.save()
                     
-                    return redirect(reverse('customer:sendgig'))
+        #             return redirect(reverse('customer:sendgig'))
                     
-                except stripe.error.CardError as e:
-                    err = e.error
-                    # Error code will be authentication_required if authentication is needed
-                    print("Code is: %s" % err.code)
-                    payment_intent_id = err.payment_intent['id']
-                    payment_intent = stripe.PaymentIntent.retrieve(payment_intent_id)
+        #         except stripe.error.CardError as e:
+        #             err = e.error
+        #             # Error code will be authentication_required if authentication is needed
+        #             print("Code is: %s" % err.code)
+        #             payment_intent_id = err.payment_intent['id']
+        #             payment_intent = stripe.PaymentIntent.retrieve(payment_intent_id)
                                 
     #Determine the current step
     if not creating_job:
         current_step = 1
-    elif creating_job.delivery_name:
-        current_step = 4
+    # elif creating_job.delivery_name:
+    #     current_step = 4
     elif creating_job.pickup_name:
         current_step = 3
     else:
@@ -162,8 +164,8 @@ def create_gig(request):
         "step": current_step,
         "step1_form": step1_form,
         "step2_form": step2_form,
-        "step3_form": step3_form,
-        "GOOGLE_MAP_API_KEY": settings.GOOGLE_MAP_API_KEY
+        # "step3_form": step3_form,
+        # "GOOGLE_MAP_API_KEY": settings.GOOGLE_MAP_API_KEY
 
     })
        
